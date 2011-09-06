3e689b155debebceb8116a6da328c952388685e6
Fedor Korotkov <fedor.korotkov@gmail.com>
2011-09-06 22:36:45 +0400

    Remove useless code.

--- src/com/intellij/plugins/haxe/lang/parser/HaxeParserDefinition.java
+++ src/com/intellij/plugins/haxe/lang/parser/HaxeParserDefinition.java
@@ -18,51 +18,42 @@ import org.jetbrains.annotations.NotNull;
 
 public class HaxeParserDefinition implements ParserDefinition {
     @NotNull
-    @Override
     public Lexer createLexer(Project project) {
         return new HaxeLexer();
     }
 
-    @Override
     public PsiParser createParser(Project project) {
         return new HaxeParser();
     }
 
-    @Override
     public IFileElementType getFileNodeType() {
         return new HaxeFileElementType();
     }
 
     @NotNull
-    @Override
     public TokenSet getWhitespaceTokens() {
         return HaxeTokenTypeSets.WHITESPACES;
     }
 
     @NotNull
-    @Override
     public TokenSet getCommentTokens() {
         return HaxeTokenTypeSets.COMMENTS;
     }
 
     @NotNull
-    @Override
     public TokenSet getStringLiteralElements() {
         return HaxeTokenTypeSets.STRINGS;
     }
 
     @NotNull
-    @Override
     public PsiElement createElement(ASTNode node) {
         return HaxePsiCreator.createElement(node);
     }
 
-    @Override
     public PsiFile createFile(FileViewProvider viewProvider) {
         return new HaxeFile(viewProvider);
     }
 
-    @Override
     public SpaceRequirements spaceExistanceTypeBetweenTokens(ASTNode left, ASTNode right) {
         return SpaceRequirements.MAY;
     }
