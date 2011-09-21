47ff5c14367c29e2cfa856ccd6d47698654ab1b0
Fedor Korotkov <fedor.korotkov@gmail.com>
2011-09-21 11:54:09 +0400

    Add if statement parsing tests.

---
+++ test/com/intellij/plugins/haxe/lang/parser/statements/IfTestCase.java
@@ -0,0 +1,16 @@
+package com.intellij.plugins.haxe.lang.parser.statements;
+
+public class IfTestCase extends StatementTestCase {
+    public void testIf$simple() throws Throwable {
+        doTest();
+    }
+
+    public void testIf$ifelse() throws Throwable {
+        doTest();
+    }
+
+    public void testIf$condition() throws Throwable {
+        doTest();
+    }
+
+}
