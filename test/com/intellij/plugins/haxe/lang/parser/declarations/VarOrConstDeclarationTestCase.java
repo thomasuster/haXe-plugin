a2ef40fea6b87b99cff4305ed816e324b48c5aae
Fedor Korotkov <fedor.korotkov@gmail.com>
2011-09-21 11:36:50 +0400

    Add variable declaration tests.

---
+++ test/com/intellij/plugins/haxe/lang/parser/declarations/VarOrConstDeclarationTestCase.java
@@ -0,0 +1,27 @@
+package com.intellij.plugins.haxe.lang.parser.declarations;
+
+public class VarOrConstDeclarationTestCase extends DeclarationTestCase {
+    public void testVariable$assign() throws Throwable {
+        doTest();
+    }
+
+    public void testVariable$assignarray() throws Throwable {
+        doTest();
+    }
+
+    public void testVariable$boolexpression() throws Throwable {
+        doTest();
+    }
+
+    public void testVariable$expression() throws Throwable {
+        doTest();
+    }
+
+    public void testVariable$simple() throws Throwable {
+        doTest();
+    }
+
+    public void testVariable$template() throws Throwable {
+        doTest();
+    }
+}
