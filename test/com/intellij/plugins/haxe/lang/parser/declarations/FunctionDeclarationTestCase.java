da2bc2453604b9617103bc3a44658aec87c468a5
Fedor Korotkov <fedor.korotkov@gmail.com>
2011-09-21 10:49:15 +0400

    Add function declaration tests.

---
+++ test/com/intellij/plugins/haxe/lang/parser/declarations/FunctionDeclarationTestCase.java
@@ -0,0 +1,27 @@
+package com.intellij.plugins.haxe.lang.parser.declarations;
+
+public class FunctionDeclarationTestCase extends DeclarationTestCase {
+    public void testFunction$badparametrs() throws Throwable {
+        doTest();
+    }
+
+    public void testFunction$constructor() throws Throwable {
+        doTest();
+    }
+
+    public void testFunction$noreturntype() throws Throwable {
+        doTest();
+    }
+
+    public void testFunction$parametr() throws Throwable {
+        doTest();
+    }
+
+    public void testFunction$parametrs() throws Throwable {
+        doTest();
+    }
+
+    public void testFunction$simple() throws Throwable {
+        doTest();
+    }
+}
