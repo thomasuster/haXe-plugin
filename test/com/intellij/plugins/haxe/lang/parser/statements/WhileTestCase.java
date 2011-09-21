503b504d2c15366177735ef49b30a163adef0fe7
Fedor Korotkov <fedor.korotkov@gmail.com>
2011-09-21 12:08:12 +0400

    Add while statement parsing tests.

---
+++ test/com/intellij/plugins/haxe/lang/parser/statements/WhileTestCase.java
@@ -0,0 +1,11 @@
+package com.intellij.plugins.haxe.lang.parser.statements;
+
+public class WhileTestCase extends StatementTestCase {
+    public void testWhile$simple() throws Throwable {
+        doTest();
+    }
+
+    public void testWhile$condition() throws Throwable {
+        doTest();
+    }
+}
